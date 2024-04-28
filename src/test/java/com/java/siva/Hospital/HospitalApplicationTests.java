88c3e2db2edc3d14555554fc96960e48d0c1983f
Siva5205 <siva5205reddy@gmail.com>
2024-04-28 13:16:54 +0530

    JavaApiSetupDone
    JavaApiSetupDone

---
+++ src/test/java/com/java/siva/Hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.siva.Hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
