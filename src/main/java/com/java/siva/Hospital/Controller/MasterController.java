f5c3b8c059bc1c8af21d6c65179c49acda5032e2
Siva5205 <siva5205reddy@gmail.com>
2024-05-31 10:33:52 +0530

    @CrossOrigin Added
    @CrossOrigin Added

--- src/main/java/com/java/siva/Hospital/Controller/MasterController.java
+++ src/main/java/com/java/siva/Hospital/Controller/MasterController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import com.java.siva.Hospital.Entity.Master;
 import com.java.siva.Hospital.Service.MasterService;
 
 @RestController
+@CrossOrigin
 public class MasterController {
 	@Autowired
 	private MasterService masterService;
